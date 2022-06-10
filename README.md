f0457b35248e672a5295cbd2491e894645c8ca1f
Kastriot Salihu <49651929+KastriotSalihu@users.noreply.github.com>
2022-06-10 23:25:47 +0200

    Updated readme optimized the generation of the possible values

--- README.md
+++ README.md
@@ -1,9 +1,52 @@
+# Sudoku Solver
+## Sudoku
+---
+
+[Sudoku](https://en.wikipedia.org/wiki/Sudoku) is a logic-based, combinatorial number-placement puzzle. 
+
+In classic Sudoku, the objective is to fill a 9 × 9 grid with digits so that each column, each row, and each of the nine 3 × 3 subgrids that compose the grid (also called "boxes", "blocks", or "regions") contain all of the digits from 1 to 9. The puzzle setter provides a partially completed grid, which for a well-posed puzzle has a single solution
+
+## Algorithm 
+---
+
+The algorithm implemented in this project represents a very natural and straightforward way to solve a sudoku.
+
+Find all the values allowed in a particular cell based on all the *groups* (row, column or box) it belongs to. 
+
+The way this is calculated is by starting from the total set of available values (numbers 1 - 9), and then removing from that set the values which are not allowed in that particular cell, (because the value is already present in another cell in the group).
+
+If a cell is left with only one possible value, that value is "bound" to that cell, and the value is removed as a possible candidate in the remaining cells of the groups.
+
+## How to Use
+---
+You can run this code using **[node](https://nodejs.org/en/)**.
+Simply go to the root directory and run 
+```
+node index.js
+```
+This will run the default sudoku found at
+```
+examples/eazy.js
+```
+and then print the solved sudoku.
+
+A very simple previewer is available at:
+```
+index.html
+```
+Simply copy the result from the solver(or any sudoku with that format).
+Then open the console.
+And run 
+```
+populateTable(sudoku)
+```
 
 
-SUDOKU TERMS:
+## Sudoku Terms:
+---
+Same value represents members of the same grouping
 
-
-ROW
+### **Row**
 
 0 0 0 | 0 0 0 | 0 0 0
 
@@ -27,9 +70,8 @@ ROW
 
 8 8 8 | 8 8 8 | 8 8 8
 
+### **Column**
 
-
-COLUMN
 
 0 1 2 | 3 4 5 | 6 7 8
 
@@ -53,10 +95,8 @@ COLUMN
 
 0 1 2 | 3 4 5 | 6 7 8
 
+### **Box**
 
-
-
-BOX
 
 0 1 2   3 4 5   6 7 8 
 
@@ -83,7 +123,7 @@ BOX
 6 6 6 | 7 7 7 | 8 8 8   8
 
 
-GROUP
+### ***Group***
 
 Row, Column or Box
 
--- src/possibleValueHelper.js
+++ src/possibleValueHelper.js
@@ -4,15 +4,7 @@ import * as groupingHelper from "./groupingHelper.js";
 /**
  * Array containing all values from 1 to MAX_NUMBER
  */
-const allValues = Array(MAX_NUMBER).fill(0).map((_, index) => index + 1);
-/**
- * Get the elements that are in this but arent in array
- * @param {Array} array 
- * @returns 
- */
-Array.prototype.notIn = function (array) {
-    return this.filter(x => !array.includes(x));
-};
+const allPossibleValues = Array(MAX_NUMBER).fill(0).map((_, index) => index + 1);
 
 export const getPossibleValues = (sudoku, row, column) => {
     // if this cell has a predefined value, it is already solved
@@ -21,33 +13,17 @@ export const getPossibleValues = (sudoku, row, column) => {
     // get box index based on row and column
     const box = groupingHelper.getBoxIndex(row, column);
 
-    // get the values that are present or not present in the groups: row, column, box
-    const { available: rowAvailable, notAvailable: rowNotAvailable } = getPossibilitiesFromGroup(groupingHelper.getRow(sudoku, row));
-    const { available: columnAvailable, notAvailable: columnNotAvailable } = getPossibilitiesFromGroup(groupingHelper.getColumn(sudoku, column));
-    const { available: boxAvailable, notAvailable: boxNotAvailable } = getPossibilitiesFromGroup(groupingHelper.getBox(sudoku, box));
-
-    // get all the possible values for this cell
-    const allPossibleValues = Array.from(new Set([
-        rowAvailable,
-        columnAvailable,
-        boxAvailable
-    ].flat()));
-
     // get all the values wich are not allowed in this cell
     const unavailableValues = Array.from(new Set([
-        rowNotAvailable,
-        columnNotAvailable,
-        boxNotAvailable
-    ].flat()));
+        // get the values that are present in the row of the cell
+        ...groupingHelper.getRow(sudoku, row).filter(x => x !== EMPTY_CELL),
+        // get the values that are present in the column of the cell
+        ...groupingHelper.getColumn(sudoku, column).filter(x => x !== EMPTY_CELL),
+        // get the values that are present in the box of the cell
+        ...groupingHelper.getBox(sudoku, box).filter(x => x !== EMPTY_CELL)
+    ]));
 
-    const availableValues = allPossibleValues.notIn(unavailableValues);
+    // filter the values that are not in the array unavailableValues
+    const availableValues = allPossibleValues.filter(x => !unavailableValues.includes(x));
     return availableValues;
-};
-
-const getPossibilitiesFromGroup = group => {
-    const groupValues = group.filter(x => x !== EMPTY_CELL);
-    return {
-        available: allValues.notIn(groupValues),
-        notAvailable: groupValues,
-    };
 };
